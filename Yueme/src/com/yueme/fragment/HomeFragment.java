b71ae95620d632df8e590b9604422670c562aa19
Lonphy <Lonphy@Lonphy-PC>
2015-03-06 14:58:48 +0800

    first merge

---
+++ Yueme/src/com/yueme/fragment/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.yueme.fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.yueme.R;
+import com.yueme.fragment.base.BaseFragment;
+
+public class HomeFragment extends BaseFragment{
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected View initView(LayoutInflater inflater) {
+		return inflater.inflate(R.layout.fragment_home, null);
+	}
+
+	@Override
+	protected void setListenerAndAdapter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
