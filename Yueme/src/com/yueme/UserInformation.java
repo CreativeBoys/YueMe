4d78af0cab7e52b250713fb3f5fc35c89ee75cf5
hsk256 <hsk256@163.com>
2015-03-08 23:08:37 +0800

    userinformation and swipebacklayout

--- Yueme/src/com/yueme/UserInformation.java
+++ Yueme/src/com/yueme/UserInformation.java
@@ -1,18 +1,260 @@
 package com.yueme;
 
-import android.app.Activity;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * @author heshaokang	
  * 2015-3-7 下午3:51:48
  */
-public class UserInformation extends Activity {
+public class UserInformation extends SwipeBackActivity implements OnClickListener{
+	private EditText nickname; 	 //昵称
+	private EditText username ;  //真实姓名
+	private EditText school;	 //学校
+	private EditText academy;    //学院
+	private EditText grade;      //年级
+	private EditText signature;  //个性签名
+	private TextView save_information;
+	private TextView backlogin;  //退出登录
+	private ImageView user_back;  //返回键
+	//头像
+	private ImageView user_head;
+	private Bitmap head;
+	private String[] items = new String[]{
+		"从相册中选取","拍照"	
+	};
+	@SuppressLint("SdCardPath")
+	private static String path = "/sdcard/myHead/"; //sd卡路径
+	
+	//请求码
+	private static final int IMAGE_REQUEST_CODE = 0;
+	private static final int CAMERA_REQUEST_CODE = 1;
+	private static final int  RESULT_REQUEST_CODE = 2;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_uer_information);
+	
+		initView();
+		initEvent();
+	}
+	
+	private void initView() {
+		user_head = (ImageView) findViewById(R.id.user_head);
+		nickname = (EditText) findViewById(R.id.nickname);
+		username = (EditText) findViewById(R.id.username);
+		school = (EditText) findViewById(R.id.school);
+		academy = (EditText) findViewById(R.id.academy);
+		grade = (EditText) findViewById(R.id.grade);
+		signature = (EditText) findViewById(R.id.signature);
+		save_information = (TextView) findViewById(R.id.save_information);
+		backlogin = (TextView) findViewById(R.id.backlogin);
+		user_back = (ImageView) findViewById(R.id.user_back);
+		Bitmap bt = BitmapFactory.decodeFile(path+"head.jpg");
+		if(bt!=null) {
+			@SuppressWarnings("deprecation")
+			Drawable drawable = new BitmapDrawable(bt); //转换成drawable
+			user_head.setImageDrawable(drawable);
+		}else {
+			//从服务器取图片，再保存到sd卡中
+		}
+	}
+	
+	private void initEvent() {
+		user_head.setOnClickListener(this);
+		save_information.setOnClickListener(this);
+		backlogin.setOnClickListener(this);
+		user_back.setOnClickListener(this);
+	}
+	
+	
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.user_head:
+			showDialog();
+			break;
+		case R.id.save_information:    //保存用户信息
+			saveInformation();
+			break;
+		case R.id.backlogin:     	  //退出登录
+			
+			break;
+		case R.id.user_back:
+			finish();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	/**
+	 * 得到用户输入的信息
+	 */
+	public void getEditText() {
+		final String nicknameEdit = nickname.getText().toString().trim();
+		final String usernameEdit = username.getText().toString().trim();
+		final String schoolEdit = school.getText().toString().trim();
+		final String academyEdit = academy.getText().toString().trim();
+		final String gradEdit = grade.getText().toString().trim();
+		final String signatureEdit = signature.getText().toString().trim();
+	}
+	
+	/**
+	 * 保存信息到服务器
+	 */
+	public void saveInformation() {
 		
 	}
+	//显示对话框
+	private void showDialog() {
+		new AlertDialog.Builder(this)
+		.setTitle("设置头像")
+		.setItems(items, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+				switch (which) {
+				case 0:
+					Intent intentFromGallery = new Intent(Intent.ACTION_PICK,null);
+					intentFromGallery.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+					startActivityForResult(intentFromGallery, IMAGE_REQUEST_CODE);
+					break;
+				case 1:
+					Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+					cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"head.jpg")));
+					startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+					break;
+				default:
+					break;
+				}
+			}
+
+			
+		}).setNegativeButton("取消", new DialogInterface.OnClickListener(){
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+			
+		}).show();
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if(resultCode!=RESULT_CANCELED) {
+			switch (requestCode) {
+			case IMAGE_REQUEST_CODE:
+				startPhotoZoom(data.getData());
+				break;
+			case CAMERA_REQUEST_CODE:
+				File temp = new File(Environment.getExternalStorageDirectory()+"/head.jpg");
+				startPhotoZoom(Uri.fromFile(temp));
+				break;
+			case RESULT_REQUEST_CODE:
+				if(data!=null) {
+					Bundle extras = data.getExtras();
+					head = extras.getParcelable("data");
+					if(head!=null) {
+						//上传到服务器
+						
+						//保存到sd卡
+						setPicToView(head);
+						user_head.setImageBitmap(head);
+					}
+					;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * 裁剪图片
+	 */
+	public void startPhotoZoom(Uri uri) {
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(uri, "image/*");
+		intent.putExtra("crop", "true");
+		//宽高比例
+		intent.putExtra("aspectX", 1);
+		intent.putExtra("aspectY",1);
+		//裁剪图片大小
+		intent.putExtra("outputX", 150);
+		intent.putExtra("outputY", 150);
+		intent.putExtra("return-data", true);
+		startActivityForResult(intent, RESULT_REQUEST_CODE);
+		
+	}
+	
+	/**
+	 * 保存裁剪后图片的数据
+	 */
+	private void setPicToView(Bitmap mBitmap) {
+		String sdStatus = Environment.getExternalStorageState();
+		//检测sd是否可用
+		if(!sdStatus.equals(Environment.MEDIA_MOUNTED)) {
+			return ;
+		}
+		FileOutputStream fos = null;
+		File file = new File(path);
+		file.mkdirs(); //创建dir
+		String fileName = path+"head.jpg";  //图片名称
+		try {
+			fos = new FileOutputStream(fileName);
+			mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos); //将数据写入文件
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}finally {
+			try {
+				fos.flush();
+				fos.close();
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+			
+		}
+		
+	
+	}
+	@Override
+	public void onBackPressed() {
+		
+		super.onBackPressed();
+		overridePendingTransition(0, R.anim.base_slide_right_out);
+	}
+	
 }
---
+++ Yueme/src/com/yueme/ui/SwipeBackLayout.java
@@ -0,0 +1,211 @@
+package com.yueme.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Scroller;
+
+import com.yueme.R;
+
+
+/**
+ * @author heshaokang	
+ * 2015-3-8 下午6:42:58
+ */
+public class SwipeBackLayout extends FrameLayout{
+	//SwipeBackLayout所在的父布局
+	private View mParentView;
+	//划定的最小距离
+	private int mTouchSlop;
+	//手指按下的位置
+	private int downX;
+	private int downY;
+	//临时存储x坐标
+	private int tempX;
+	private Scroller mScroller;
+	private int viewWidth;
+	//是否滑动
+	private boolean isSliding;
+	//是否关闭
+	private boolean isFinish;
+	//处理滑动逻辑的view
+	private Activity mActivity;
+	private Drawable mShadowDrawable;
+	/**
+	 * @param context
+	 * @param attrs
+	 * @param defStyleAttr
+	 */
+	public SwipeBackLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+		mScroller = new Scroller(context);
+		mShadowDrawable = getResources().getDrawable(R.drawable.shadow_left);
+	}
+	
+	public void attachToActivity(Activity activity) {
+		mActivity = activity;
+		TypedArray a = activity.getTheme().obtainStyledAttributes(
+				new int[] { android.R.attr.windowBackground });
+		int background = a.getResourceId(0, 0);
+		a.recycle();
+
+		ViewGroup decor = (ViewGroup) activity.getWindow().getDecorView();
+		ViewGroup decorChild = (ViewGroup) decor.getChildAt(0);
+		decorChild.setBackgroundResource(background);
+		decor.removeView(decorChild);
+		addView(decorChild);
+		setContentView(decorChild);
+		decor.addView(this);
+	}
+	private void setContentView(View decorChild) {
+		mParentView = (View) decorChild.getParent();
+	}
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public SwipeBackLayout(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	/**
+	 * @param context
+	 */
+	public SwipeBackLayout(Context context) {
+		super(context);
+	}
+	
+	/**
+	 * 事件拦截操作
+	 */
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+		switch (ev.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			downX = tempX = (int) ev.getRawX();
+			downY = (int) ev.getRawY();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			int moveX = (int) ev.getRawX();
+			// 满足此条件屏蔽SildingFinishLayout里面子类的touch事件
+			if (moveX - downX > mTouchSlop
+					&& Math.abs((int) ev.getRawY() - downY) < mTouchSlop) {
+				return true;
+			}
+			break;
+		}
+
+		return super.onInterceptTouchEvent(ev);
+	}
+	
+	
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right,
+			int bottom) {
+		
+		super.onLayout(changed, left, top, right, bottom);
+		if(changed) {
+			
+			viewWidth = this.getWidth();
+		}
+		
+	}
+	
+	
+	/**
+	 * 滑出屏幕 
+	 */
+	private void scrollRight() {
+		final int delta = (int) (viewWidth+mParentView.getScaleX());
+		//设置滚动参数
+		mScroller.startScroll(mParentView.getScrollX(), 0, -delta+1, 0,Math.abs(delta));
+		postInvalidate();
+	}
+	
+	/**
+	 * 回复原状
+	 */
+	private void scrollOrigin() {
+		int delta = mParentView.getScrollX();
+		mScroller.startScroll(mParentView.getScrollX(), 0, -delta,0, Math.abs(delta));
+		postInvalidate();
+	}
+	
+	@Override
+	protected void dispatchDraw(Canvas canvas) {
+		super.dispatchDraw(canvas);
+		if (mShadowDrawable != null && mParentView != null) {
+
+			int left = mParentView.getLeft()
+					- mShadowDrawable.getIntrinsicWidth();
+			int right = left + mShadowDrawable.getIntrinsicWidth();
+			int top = mParentView.getTop();
+			int bottom = mParentView.getBottom();
+
+			mShadowDrawable.setBounds(left, top, right, bottom);
+			mShadowDrawable.draw(canvas);
+		}
+
+	}
+	
+	@Override
+	public void computeScroll() {
+		//调用startScroll的时候 scroller.computeScrollOffset返回true
+		if(mScroller.computeScrollOffset()) {
+			//调用scrollerTo() 来更新子view 位置
+			mParentView.scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+			postInvalidate();
+			if (mScroller.isFinished() && isFinish) {
+				mActivity.finish();
+			}
+		}
+		
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			downX = tempX = (int) event.getRawX();
+			downY = (int) event.getRawY();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			int moveX = (int) event.getRawX();
+			int deltaX = tempX-moveX;
+			tempX = moveX;
+			if(moveX-downX>mTouchSlop && Math.abs(event.getRawY()-downY)<mTouchSlop) {
+				isSliding = true;
+			}
+			if(moveX-downX>=0 && isSliding) {
+				mParentView.scrollBy(deltaX,0);
+				
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+			isSliding = false;
+			if(mParentView.getScrollX()<= -viewWidth/2) {
+				isFinish = true;
+				//滑出屏幕
+				scrollRight();
+			}else {
+				isFinish = false;
+				//回复原状
+				scrollOrigin();
+			}
+			
+			break;
+		}
+		return true;
+	}
+	
+}
